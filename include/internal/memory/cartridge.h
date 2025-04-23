724c5c6996843510c0481565ba84360549a0b19f
Vanzha Kirill <vanzha.kirill@gmail.com>
2025-04-23 14:29:00 +0300

    Add cartridge definitions and such

---
+++ include/internal/memory/cartridge.h
@@ -0,0 +1,58 @@
+#ifndef CARTRIDGE_H
+#define CARTRIDGE_H
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+
+struct cartridge_header {
+    uint8_t entry[4];
+    uint8_t logo[48];
+    union {
+        char old_title[15]; // not necessarily null-terminated
+        struct {
+            char new_title[11]; // not necessarily null-terminated
+            char manufacturer[4];
+        };
+    };
+    uint8_t cgb;
+    char new_licensee_code[2];
+    uint8_t sgb;
+    uint8_t cartridge_type;
+    uint8_t rom_size;
+    uint8_t ram_size;
+    uint8_t destination_code;
+    uint8_t old_licensee_code;
+    uint8_t version;
+    uint8_t header_checksum;
+    uint16_t global_checksum;
+};
+
+enum cartridge_mapper_type { CMT_ROM_ONLY, CMT_UNSUPPORTED = -1 };
+
+#define CARTRIDGE_ROM_BANK_SIZE 16384
+
+struct cartridge {
+    enum cartridge_mapper_type mapper;
+    struct cartridge_header *header;
+    uint8_t bank_00[CARTRIDGE_ROM_BANK_SIZE];
+    uint8_t bank_01_nn[CARTRIDGE_ROM_BANK_SIZE];
+};
+
+// Constructs cartridge rom data from a file.
+struct cartridge *cartridge_new(const char *fname);
+
+// Deallocates cartridge and all the memory associated with it.
+void cartridge_delete(struct cartridge *cart);
+
+// Prints header.
+void cartridge_header_print_info(const struct cartridge_header *cart,
+                                 FILE *out);
+
+// Reads data from rom of a cartridge.
+uint8_t cartridge_rom_read(struct cartridge *cart, uint16_t address);
+
+// Writes data to a rom of a cartridge.
+void cartridge_rom_write(struct cartridge *cart, uint16_t address, uint8_t val);
+
+#endif
---
+++ src/internal/memory/cartridge.c
@@ -0,0 +1,101 @@
+#include "internal/memory/cartridge.h"
+
+#include <assert.h>
+#include <stdlib.h>
+
+static enum cartridge_mapper_type cartridge_mapper_type(uint8_t cart_type) {
+    switch (cart_type) {
+    case 0x00:
+        return CMT_ROM_ONLY;
+    default:
+        return CMT_UNSUPPORTED;
+    }
+}
+
+struct cartridge *cartridge_new(const char *fname) {
+    assert(fname != NULL);
+
+    struct cartridge *cart = malloc(sizeof(struct cartridge));
+    assert(cart != NULL);
+
+    FILE *file = fopen(fname, "r");
+    assert(file != NULL);
+
+    fseek(file, 0, SEEK_END);
+    size_t file_size = ftell(file);
+    rewind(file);
+
+    assert(file_size == 2 * CARTRIDGE_ROM_BANK_SIZE);
+
+    fread(cart->bank_00, CARTRIDGE_ROM_BANK_SIZE, sizeof(uint8_t), file);
+    fread(cart->bank_01_nn, CARTRIDGE_ROM_BANK_SIZE, sizeof(uint8_t), file);
+    cart->header = (void *)(cart->bank_00 + 0x0100);
+
+    fclose(file);
+
+    cart->mapper = cartridge_mapper_type(cart->header->cartridge_type);
+    assert(cart->mapper != CMT_UNSUPPORTED);
+
+    return cart;
+}
+
+void cartridge_delete(struct cartridge *cart) {
+    if (cart == NULL) {
+        return;
+    }
+
+    switch (cart->mapper) {
+    case CMT_ROM_ONLY:
+        free(cart);
+        return;
+    case CMT_UNSUPPORTED:
+        exit(1);
+    }
+}
+
+void cartridge_header_print_info(const struct cartridge_header *cart,
+                                 FILE *out) {
+    assert(cart != NULL);
+    assert(out != NULL);
+
+    fprintf(out, "Old title: %.16s\n", cart->old_title);
+    fprintf(out, "New title: %.16s\n", cart->new_title);
+    fprintf(out, "Manufacturer: %.16s\n", cart->manufacturer);
+    fprintf(out, "CGB: %u\n", (unsigned int)cart->cgb);
+    fprintf(out, "New lic code: %.2s\n", cart->new_licensee_code);
+    fprintf(out, "SGB: %u\n", (unsigned int)cart->sgb);
+    fprintf(out, "Cart type: %u\n", (unsigned int)cart->cartridge_type);
+    fprintf(out, "Rom size: %u\n", (unsigned int)cart->rom_size);
+    fprintf(out, "Ram size: %u\n", (unsigned int)cart->ram_size);
+    fprintf(out, "Dest code: %u\n", (unsigned int)cart->destination_code);
+    fprintf(out, "Old lic code: %u\n", (unsigned int)cart->old_licensee_code);
+    fprintf(out, "Version: %u\n", (unsigned int)cart->version);
+    fprintf(out, "Header checksum: %u\n", (unsigned int)cart->header_checksum);
+    fprintf(out, "Global checksum: %u\n", (unsigned int)cart->global_checksum);
+}
+
+uint8_t cartridge_rom_read(struct cartridge *cart, uint16_t address) {
+    assert(cart != NULL);
+    assert(0x0000 <= address && address <= 0x7FFF);
+
+    switch (cart->mapper) {
+    case CMT_ROM_ONLY:
+        return address >= 0x4000 ? cart->bank_01_nn[address - 0x4000]
+                                 : cart->bank_00[address - 0x0000];
+    case CMT_UNSUPPORTED:
+        exit(1);
+    }
+}
+
+void cartridge_rom_write(struct cartridge *cart, uint16_t address,
+                         uint8_t val) {
+    assert(cart != NULL);
+    assert(0x0000 <= address && address <= 0x7FFF);
+
+    switch (cart->mapper) {
+    case CMT_ROM_ONLY:
+        return;
+    case CMT_UNSUPPORTED:
+        exit(1);
+    }
+}
